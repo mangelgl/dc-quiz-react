d25f15f3b1ac0053ed0cc52186d4c2eebb766e5b
magarcial <magarcial@minsait.com>
2024-08-22 14:33:55 +0200

    Create new components + styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import './index.css';
+import Quiz from './components/Quiz';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-);
+root.render(<Quiz />);
+
+// React.StrictMode => show errors if something goes wrong
+// also render the app twice
+// <React.StrictMode></React.StrictMode>
+
