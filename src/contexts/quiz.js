7934a78d00d62f655a919d11dc938dc03b47bf29
magarcial <magarcial@minsait.com>
2024-08-23 14:51:14 +0200

    Render answers & Highlighting answers

--- src/contexts/quiz.js
+++ src/contexts/quiz.js
@@ -1,22 +1,48 @@
 import { createContext, useReducer } from 'react';
 import questions from '../data';
+import { shuffleAnswers } from '../helpers';
 
 const initialState = {
 	currentQuestionIndex: 0,
 	questions,
 	showResults: false,
+	answers: shuffleAnswers(questions[0]),
+	currentAnswer: '',
+	correctAnswersCount: 0,
 };
 
 const reducer = (state, action) => {
-	console.log('render', state);
 	switch (action.type) {
+		case 'SELECT_ANSWER': {
+			const correctAnswersCount =
+				action.payload ===
+				state.questions[state.currentQuestionIndex].correctAnswer
+					? state.correctAnswersCount + 1
+					: state.correctAnswersCount;
+			return {
+				...state,
+				currentAnswer: action.payload,
+				correctAnswersCount,
+			};
+		}
+
 		case 'NEXT_QUESTION': {
 			const showResults =
 				state.currentQuestionIndex === state.questions.length - 1;
 			const currentQuestionIndex = showResults
 				? state.currentQuestionIndex
 				: state.currentQuestionIndex + 1;
-			return { ...state, currentQuestionIndex, showResults };
+			const answers = showResults
+				? []
+				: shuffleAnswers(state.questions[currentQuestionIndex]);
+
+			return {
+				...state,
+				currentQuestionIndex,
+				showResults,
+				answers,
+				currentAnswer: '',
+			};
 		}
 
 		case 'RESTART_QUIZ': {
@@ -32,6 +58,5 @@ export const QuizContext = createContext();
 
 export const QuizProvider = ({ children }) => {
 	const value = useReducer(reducer, initialState);
-	console.log('state', value);
 	return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>;
 };
